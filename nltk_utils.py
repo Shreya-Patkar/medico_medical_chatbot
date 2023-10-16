3e92012a404242ceae83811079e09424d9663e48
Shreya-Patkar <shreya.patkar19@gmail.com>
2023-10-16 23:53:39 +0530

    Update nltk_utils.py

--- nltk_utils.py
+++ nltk_utils.py
@@ -4,6 +4,25 @@ import nltk
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
+#Tokenization of sentences done
+def tokenize(sentence):
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    """
+    stemming = find the root form of the word
+    examples:
+    words = ["organize", "organizes", "organizing"]
+    words = [stem(w) for w in words]
+    -> ["organ", "organ", "organ"]
+    """
+    return stemmer.stem(word.lower())
+
 
 
 
