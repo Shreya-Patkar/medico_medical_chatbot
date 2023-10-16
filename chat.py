e0784235534a03b447c4f853475d70464a284579
chicken-burgerrrr <138805573+chicken-burgerrrr@users.noreply.github.com>
2023-10-16 22:57:19 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -33,17 +33,7 @@ def get_response(msg):
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
 
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
-
-    tag = tags[predicted.item()]
-
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
-    if prob.item() > 0.75:
-        for intent in intents['intents']:
-            if tag == intent["tag"]:
-                return random.choice(intent['responses'])
+    
     
     return "I do not understand..."
 
